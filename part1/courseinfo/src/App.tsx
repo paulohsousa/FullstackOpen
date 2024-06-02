53c27cfa97c62b9be963f080c72a959cec669dd6
Paulo Henrique de Sousa <paulo_henrique-16@live.com>
2024-06-02 11:54:39 -0300

    Part 1.1 and 1.2 of 1

---
+++ part1/courseinfo/src/App.tsx
@@ -0,0 +1,67 @@
+const Header = () => {
+  const course = 'Half stack application development'
+  return(
+    <h1>{course}</h1>
+  )
+}
+const Part = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = '14'
+  return(
+    <div>
+      <p>
+    {part1} {exercises1}
+  </p>
+   <p>
+   {part2} {exercises2}
+ </p>
+  <p>
+  {part3} {exercises3}
+</p>
+    </div>
+    
+  )
+}
+const Content = () => {
+  
+ 
+ 
+  
+  return(
+    <div>
+    <p>
+    <Part ></Part>
+  </p>
+  
+  </div>
+  )
+}
+const Total = () => {
+  const exercises1 = 10
+ 
+  const exercises2 = 7
+ 
+  const exercises3 = '14'
+  return(
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  )
+}
+
+const App = () => {
+ 
+  
+ 
+  return (
+    
+    <div>
+     <Header></Header>
+     <Content></Content>
+     <Total></Total>
+    </div>
+  )
+}
+export default App
